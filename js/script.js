6cdea5601fa4c562bc11ae151928a244e8f1d65e
Albert Chung <chung972@gmail.com>
2019-05-03 08:27:51 -0500

    more todo comments

--- js/script.js
+++ js/script.js
@@ -12,6 +12,51 @@ var board, winner, turn;
 
 // TODO need variables that will hold/count each player's chips
 //      maybe just use a nested forEach combined with .includes(turn)?
+/**
+ *   TODO need a boolean var to track whether a move is legal? would need
+     an accompanying function; would we need to keep it global if it 
+     would only live in handleClick? maybe need an entirely new function
+     handleHover? might be easier just to set borderstyle based on whether or not
+     PLAYERS[content] === 0 (plus other conditions)
+     in fact, it's looking more and more likely that we WILL need a separate eventListener
+     (it's called mouseENTER not hover; hover is css); when you ENTER, check if that move is
+     legal depending on CURRENT turn
+
+     maybe need to make checkCol/Row/Fwd-BckDiags more general? maybe just return true?, then
+     depending on that we can perform whatever action. i say this so we can stay DRY; 
+     well you wouldn't be able to click at all in the first place if a move was illegal
+     when you CLICK (which, again, can ONLY HAPPEN if a move is LEGAL), you are firstly,
+     setting that div/tile to hold a value of the CURRENT TURN; you are also calling on render
+     to reflect that update in the app state; the click should then call on a function to convert
+     whatever 
+
+     OH BITCH. LISTEN. so. we have a board array, right? and we have a handle on that fucker.
+     so. just have an array holding other arrays? or maybe just the coordinates? when we call on 
+     checkCol/Row/Diags, we will KNOW what turn value the tiles we iterate through will have.
+     so while we're iterating through them, store the enemy chips while we search for one of our own
+     (you get what i'm saying. until we find another of the current turn's player's color)
+     and so, depending on whether certain conditions are met, we can simply update the BOARD VAR ITSELF;
+     that is, change the values at whichever positions we were holding in that array; then call render on that
+     bitch. qed? remember, RENDER() IS RESPONSIBLE FOR ALL(?) FRONT END SHIT. WE DON'T NEED A SPECIFIC RENDER
+     FUCNTION FOR CONVERTING TILES. we WILL, however, need a specific function to change the app state of the 
+     board var (converting them, that is)
+     
+
+
+     you necessarily have to ENTER the div you're about to click in order to click it. so
+     on mouseENTER, you'll be calling on isLegal(), this will check whether there is an aligning tile on
+     the vert/horiz/diag axes  
+     have the 
+
+     maybe, while you're storing the positions of the enemy tiles while you're checking for your own,
+     store the positions of the enemy tiles with the same naming convention as their div counterpart ids,
+     that is, like c(col#)r(row#), that way, we can use these two lines to parse them back after we've stored
+     them. 
+        const colIdx = parseInt(tile.id.charAt(1));
+        const rowIdx = parseInt(tile.id.charAt(3));
+ *  */
+
+
 
 /*----- cached element references -----*/
 
